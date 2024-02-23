bc41b4d25375398efb871a8224a4bd2b0fceee50
Edu Moreno <e.m.morenolp@gmail.com>
2024-02-22 23:27:12 -0300

    ultimas prubas sin completar

--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -161,8 +161,8 @@ const config = {
       }
     }
   },
-  "inlineSchema": "// This is your Prisma schema file,\n// learn more about it in the docs: https://pris.ly/d/prisma-schema\n\n// Looking for ways to speed up your queries, or scale easily with your serverless or edge functions?\n// Try Prisma Accelerate: https://pris.ly/cli/accelerate-init\n\ndatasource db {\n  provider = \"postgresql\"\n  url = env(\"POSTGRES_PRISMA_URL\") // uses connection pooling\n  directUrl = env(\"POSTGRES_URL_NON_POOLING\") // uses a direct connection\n}\n\ngenerator client {\n  provider = \"prisma-client-js\"\n  output   = \"./generated/client\"\n}\n\nmodel Task {\n  id          Int       @id @default(autoincrement())\n  title       String\n  description String?\n  createdAt   DateTime  @default(now())\n}\n",
-  "inlineSchemaHash": "c96c30157c1f0df81b1ba50275b8a2122f41d8ea906e15a191c108369aeb9574",
+  "inlineSchema": "generator client {\n  provider = \"prisma-client-js\"\n  output   = \"./generated/client\"\n}\n\ndatasource db {\n  provider  = \"postgresql\"\n  url       = env(\"POSTGRES_PRISMA_URL\")\n  directUrl = env(\"POSTGRES_URL_NON_POOLING\")\n}\n\nmodel Task {\n  id          Int      @id @default(autoincrement())\n  title       String\n  description String?\n  createdAt   DateTime @default(now())\n}\n",
+  "inlineSchemaHash": "1acb0a590cf6cbdb1593950290f6de85152f67358b3f9b1cc9388fc0042928b3",
   "copyEngine": true
 }
 
